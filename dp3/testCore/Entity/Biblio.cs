b81ae7b4fb17c21544562d5605e450e41e8e681c
renyh <renyh1013@163.com>
2018-05-07 18:18:06 +0800

    加入测试代码

---
+++ dp3/testCore/Entity/Biblio.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+
+namespace testCore.Entity
+{
+    public class Biblio
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id { get; set; }
+
+        public List<Identifier> identifiers { get; set; }
+    }
+
+    public class Identifier
+    {
+        public string type { get; set; }
+        public string no { get; set; }
+        public double price { get; set; }
+        public List<string> errorNo { get; set; }
+    }
+
+}
